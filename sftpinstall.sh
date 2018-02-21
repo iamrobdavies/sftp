331fd3872cc362c85419657349dff388cad5014e
Rob Davies <rob.davies@outlook.com>
2018-02-20 23:10:52 -0800

    Update sftpinstall.sh

--- sftpinstall.sh
+++ sftpinstall.sh
@@ -11,23 +11,26 @@ do
 done
 
 sudo addgroup ftpaccess
+sudo useradd -m sftpuser -s /usr/sbin/nologin -G ftpaccess
+echo "sftpuser:$USERPASS" | sudo chpasswd
+
+sudo mkdir -p /sftp/ecom
+sudo chmod 755 /sftp
+sudo chown root:root /sftp
+sudo chown -R sftpuser:ftpaccess /sftp/ecom
 
 sudo sed -i '/Subsystem sftp/s/^/#/g' /etc/ssh/sshd_config
 
 echo $'\nSubsystem sftp internal-sftp' | sudo tee --append /etc/ssh/sshd_config > /dev/null
-echo "Match group ftpaccess" | sudo tee --append /etc/ssh/sshd_config > /dev/null
-echo "ChrootDirectory %h" | sudo tee --append /etc/ssh/sshd_config > /dev/null
-echo "X11Forwarding no" | sudo tee --append /etc/ssh/sshd_config > /dev/null
-echo "AllowTcpForwarding no" | sudo tee --append /etc/ssh/sshd_config > /dev/null
-echo "ForceCommand internal-sftp" | sudo tee --append /etc/ssh/sshd_config > /dev/null
+echo "Match User sftpuser" | sudo tee --append /etc/ssh/sshd_config > /dev/null
+echo $'\tChrootDirectory /sftp' | sudo tee --append /etc/ssh/sshd_config > /dev/null
+echo $'\tX11Forwarding no' | sudo tee --append /etc/ssh/sshd_config > /dev/null
+echo $'\tAllowTcpForwarding no' | sudo tee --append /etc/ssh/sshd_config > /dev/null
+echo $'\tForceCommand internal-sftp' | sudo tee --append /etc/ssh/sshd_config > /dev/null
+echo $'\tPasswordAuthentication yes' | sudo tee --append /etc/ssh/sshd_config > /dev/null
+echo "Match all" | sudo tee --append /etc/ssh/sshd_config > /dev/null
 
 sudo systemctl restart ssh
-
-sudo useradd -m sftpuser -s /usr/sbin/nologin -G ftpaccess
-echo "sftpuser:$USERPASS" | sudo chpasswd
-sudo chown root:root /home/sftpuser
-sudo mkdir -p /home/sftpuser/ecom
-sudo chown -R sftpuser:ftpaccess /home/sftpuser/ecom
 
 until sudo apt-get -y update && sudo apt-get -y install cifs-utils
 do
@@ -35,7 +38,6 @@ do
 	sleep 2
 done
 
-sudo mkdir -p /datadir
-sudo bash -c 'echo "//$0.file.core.windows.net/$1	/datadir	cifs	nofail,vers=3.0,username=$0,password=$2,dir_mode=0777,file_mode=0777,serverino" >> /etc/fstab' $ACCOUNT $CONTAINER $STORAGEPASS
+sudo bash -c 'echo "//$0.file.core.windows.net/$1	/home/sftpuser/ecom	cifs	nofail,vers=3.0,username=$0,password=$2,dir_mode=0777,file_mode=0777,serverino" >> /etc/fstab' $ACCOUNT $CONTAINER $STORAGEPASS
 
 sudo mount -a
